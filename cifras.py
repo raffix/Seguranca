f634b89c6862287feefaf127786853d61c299aa8
raffixrcr <raffixrcr@hotmail.com>
2015-08-11 01:07:03 -0300

    Metade concluida

---
+++ cifras.py
@@ -0,0 +1,119 @@
+dados = []
+
+def readFile():
+	entrada = raw_input('Informe o arquivo de entrada:\t ')
+	arquivo = open(entrada,'rb')
+	global dados
+	data = arquivo.readlines()
+	for line in data:
+		for c in line:
+			dados.append(c)
+
+	arquivo.close()
+	
+
+def ceasar(chave):
+	readFile()
+	saida = raw_input("Informe o arquivo de saida:\t ")
+	arquivo = open(saida,'wb')
+	''' map(charPlus,dados) '''
+	for c in dados:
+		caracter = ord(c) + chave
+		caracter = caracter % 256
+		arquivo.write(chr(caracter))
+
+	arquivo.close()
+
+	print "\n\tConcluido Caesar\n"
+
+	''' descriptografa '''
+
+	arquivo = open(saida,'rb')
+	saida2 = 'DESCRIPTOGRAFADO'+saida
+	arquivo2 = open(saida2,'wb')
+	dadosCrip = arquivo.readlines()
+	for line in dadosCrip:
+		for c in line:
+			caracter = int(ord(c)) - chave
+			caracter = caracter % 256
+			arquivo2.write(chr(caracter))
+
+	arquivo.close()
+	arquivo2.close()
+
+
+def transposicao(chave):
+	readFile()
+	matriz = []
+	saida = raw_input("Informe o arquivo de saida:\t ")
+	arquivo = open(saida,'wb')
+	lista = []
+	for line in dados:
+		for c in line:
+			lista.append(c)
+
+	restChar = len(lista) % chave
+	if( restChar != 0 ):
+		c = 0
+		''' Adiciona os caracteres para fechar a matriz '''
+		while c < restChar:
+			lista.append('')
+			c = c +1
+	
+	restChar = len(lista) / chave
+	matriz = []
+	pos = 0
+	
+	''' Gera a matriz '''
+	for c in range(0,restChar):
+		linha = []
+		for d in range(0,chave):
+			linha.append(lista[pos])
+			pos = pos + 1
+		matriz.append(linha)
+
+	''' Transposicao da matriz e gravacao '''
+	for d in range(0,chave):
+		for c in range(0,restChar):
+			arquivo.write(matriz[c][d])
+
+
+def vigenere(chave):
+	readFile()
+	saida   = raw_input("Informe o arquivo de saida:\t ")
+	arquivo = open(saida+".encrypted",'wb')
+	nvezes  = len(dados) / len(chave)
+	resto   = len(dados) % len(chave)
+	print resto
+	if( resto !=  0 ):
+		
+
+
+
+def main():
+	metodo = int(raw_input("\n Escolha o metodo de criptografia: \n\t1 - Cifra de Caesar \n\t2 - Cifra de transposicao \n\t3 - Cifra de vigenere\n\t4 - Cifra de substituicao\nEntrada: "))
+
+	if metodo == 1 :
+		chave = int(raw_input("\n\n\t CIFRA DE CAESAR \nInforme o valor da chave:\t "))
+		ceasar(chave)
+
+	elif metodo == 2 :
+		chave = int(raw_input("\n\n\n Cifra de transposicao \n\n\nInforme o valor da chave: "))
+		transposicao(chave)
+
+	elif metodo == 3 :
+		chave = raw_input("\n\n\n Cifra de vigenere \n\n\nInforme o valor da chave: ")
+		vigenere(chave)
+	elif metodo == 4 :
+		chave = 256
+		print "\n\n\n Cifra de substituicao \n\n\nTamanho do alfabeto: "
+
+	else :
+		print "\n\n\nNenhuma cifra valida escolhida \n "
+		return
+
+
+
+		
+
+main()
